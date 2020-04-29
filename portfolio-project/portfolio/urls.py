ded811f648792a720b099ad3ea4f7f20f0bdd674
MJTMace <17456158+MJTMace@users.noreply.github.com>
2020-04-29 18:33:25 +0100

    Created Hello World page

--- portfolio-project/portfolio/urls.py
+++ portfolio-project/portfolio/urls.py
@@ -15,5 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import examples.views
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("home", examples.views.home, name="home"),
+]
