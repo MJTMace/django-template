1f6c18979913e7989b91bd46484cd934be84c55d
MJTMace <17456158+MJTMace@users.noreply.github.com>
2020-05-02 15:47:23 +0100

    adapting model for database to include more details for example projects

--- portfolio-project/examples/models.py
+++ portfolio-project/examples/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import os
+from django.conf import settings
 
 # Create your models here.
 
@@ -9,5 +11,18 @@ class Example(models.Model):
     # Summary (text explanation):
     summary = models.CharField(max_length=200)
 
+    # Other images for the individual example's detail page:
+    imagedetail1 = models.ImageField(
+        upload_to="images/",
+        default=os.path.join(settings.STATIC_ROOT, "images", "photo_MM.jpg"),
+    )
+    imagedetail2 = models.ImageField(
+        upload_to="images/",
+        default=os.path.join(settings.STATIC_ROOT, "images", "photo_MM.jpg"),
+    )
+    # More detailed text summary:
+    summarydetail = models.CharField(max_length=1500, default="")
+
+    # Display example summary in Django admin instead of 1,2,3...
     def __str__(self):
         return self.summary
